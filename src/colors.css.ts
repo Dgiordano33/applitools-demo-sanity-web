47f19fde0d06c5188884ea976b8afb69d80844fa
Daniel Giordano <danielgiordano@Daniels-MacBook-Pro.local>
2022-09-22 14:27:18 -0400

    new upload

--- src/colors.css.ts
+++ src/colors.css.ts
@@ -1,7 +1,7 @@
 export const colors = {
-  background: "#f03e2f",
-  text: "#000000",
-  primary: "#000000",
+  background: "#fff",
+  text: "#000",
+  primary: "#000",
   muted: "#f14e41",
   active: "#e6e6e6",
   black: "#000",
